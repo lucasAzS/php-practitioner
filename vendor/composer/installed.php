402954006ecf18c48e6cc33b5f3b03e229d0b6a5
Lucas Azambuja Santos <lucasazsantos@gmail.com>
2021-06-05 20:29:19 -0300

    using composer for autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3a8d6c2fb3be87ba9e2ea4c0043503ca04aa2adb',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3a8d6c2fb3be87ba9e2ea4c0043503ca04aa2adb',
+    ),
+  ),
+);
